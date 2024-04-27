2d213a9e92a18f60eea9ea2474ed2bdb05111cdc
zmgong <ming2280089874@gmail.com>
2024-04-27 16:01:18 -0700

    Add code for split data and check the splitted data.

---
+++ check_splited_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import argparse
+from collections import Counter
+import numpy as np
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--splitted_metadata_path', type=str, default="BIOSCAN_metadata/BIOSCAN_5M_Insect_Dataset_metadata_splitted.tsv",)
+
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.splitted_metadata_path, sep='\t')
+
+    print(df['split'].value_counts())
+    exit()
