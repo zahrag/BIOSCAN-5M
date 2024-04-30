40183cc3ad1617aa8f08e41a93780c0e288a0efd
zahrag <zahra.gharaee@gmail.com>
2024-04-30 13:21:27 -0400

    update main

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 
-from bioscan_statistic import show_statistics
-from bioscan_distribution import show_distributions
-from bioscan_split_statistic import show_split_statistics
-from bioscan_configuration import set_config
+from BIOSCAN_DATASET.bioscan_datadownload import make_download
+from BIOSCAN_DATASET.bioscan_statistic import show_statistics
+from BIOSCAN_DATASET.bioscan_distribution import show_distributions
+from BIOSCAN_DATASET.bioscan_split import make_split
+from BIOSCAN_DATASET.bioscan_split_statistic import show_split_statistics
+from BIOSCAN_DATASET.config.data_config import set_config
+
+from BIOSCAN_CLIP.scripts.train_cl import train_bioscan_clip
+from BIOSCAN_CLIP.scripts.inference_and_eval import test_bioscan_clip
 
 
 if __name__ == '__main__':
 
-    # ################################# DATASET & MODEL CONFIGURATIONS ##########################################
     configs = set_config()
+    make_download(configs)
 
-    # ################################# GET DATASET STATISTICS #############################################
     show_statistics(configs)
-
-    # ################################# GET DATASET DISTRIBUTIONS ##########################################
     show_distributions(configs)
 
-    # ################################# GET SPLIT DISTRIBUTIONS ##########################################
+    make_split(configs)
     show_split_statistics(configs)
+
+    train_bioscan_clip(configs)
+    test_bioscan_clip(configs)
+
 
 
 
